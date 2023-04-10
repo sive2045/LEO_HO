60a11ac370e997e3fcf64268748e2cdb8da6b695
sive2045 <lc9902130509@gmail.com>
2023-04-11 03:46:26 +0900

    conf: DQN args

--- train_DQN.py
+++ train_DQN.py
@@ -44,9 +44,9 @@ def get_parser() -> argparse.ArgumentParser:
         help='Number of groundstations(agents) in the env'
     )
     parser.add_argument('--n-step', type=int, default=10)
-    parser.add_argument('--target-update-freq', type=int, default=100)
+    parser.add_argument('--target-update-freq', type=int, default=1000)
     parser.add_argument('--epoch', type=int, default=1_000_000)
-    parser.add_argument('--step-per-epoch', type=int, default=1000)
+    parser.add_argument('--step-per-epoch', type=int, default=10_000)
     parser.add_argument('--step-per-collect', type=int, default=10)
     parser.add_argument('--update-per-step', type=float, default=0.1)
     parser.add_argument('--batch-size', type=int, default=64)
